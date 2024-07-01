3e0c978b59db46b59976fe49342ae0f452038653
Julia Castro <julia.gabrielly2011@gmail.com>
2024-06-30 21:03:25 -0300

    build(intra): :sparkles: creating a mongodb service
    creating a mongodb service and consuming in the go service

--- ms-go/db/database.go
+++ ms-go/db/database.go
@@ -15,28 +15,34 @@ func Connection() *mongo.Collection {
 	var err error
 
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://admin:password@mongodb:27017")
 
 	// Connect to MongoDB
 	client, err = mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
-		fmt.Println("MONGO: ", err)
+		fmt.Println("Failed to connect to MongoDB: ", err)
 		return nil
 	}
 
 	// Check the connection
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
-		fmt.Println("MONGO: ", err)
+		fmt.Println("Failed to ping MongoDB: ", err)
 		return nil
 	}
 
 	// Set the database and collection variables
 	db = client.Database("teste_backend").Collection("products")
 
+	fmt.Println("Connected to MongoDB!")
+
 	return db
 }
 
 func Disconnect() {
-	client.Disconnect(context.TODO())
+    err := client.Disconnect(context.Background())
+    if err != nil {
+        fmt.Println("Failed to disconnect from MongoDB:", err)
+    }
 }
+
